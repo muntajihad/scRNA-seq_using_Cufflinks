7e9e3eb616104da6cc6e0849c21cf2014a8cf95c
muntajihad <73559402+muntajihad@users.noreply.github.com>
2021-04-16 20:31:55 +0300

    Update cuffnorm.sh

--- cuffnorm.sh
+++ cuffnorm.sh
@@ -2,7 +2,7 @@
 
 gtf=/path/to/GENCODE.gtf
 
-cuffnorm --no-update-check -p 8 --use-sample-sheet -o final_out GENCODE.gtf sample_sheet.txt
+cuffnorm --no-update-check -p 8 --use-sample-sheet -o final_out $gtf sample_sheet.txt
 
 # This will generate "final_out" folder containing multiple files that are ready to be used in R (by Monocole)
 # Monocle: https://cole-trapnell-lab.github.io/monocle3/

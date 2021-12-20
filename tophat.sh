1da589af5d57ef8b25999e3c822c82eca6a83c17
muntajihad <73559402+muntajihad@users.noreply.github.com>
2021-12-20 22:47:46 +0300

    Update tophat.sh

--- tophat.sh
+++ tophat.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# working directory is RNA_trimmed_data
+# paired end samples are named as: cell_##_R1.fastq.gz and cell_##_R2.fastq.gz
+
 gtf=/path/to/GENCODE.gtf
 genome=/path/to/reference_genome #indexed by Bowtie
 
-for i in (ls *.gz | cut -5 | uniq);
+for i in (ls *.gz | cut -c -5 | uniq);
 do
-tophat -p 8 -o ${i}_topout -G $gtf $genome ${i}R1.fastq.gz {i}R2.fastq.gz;
+tophat -p 8 -o ${i}topout -G $gtf $genome ${i}R1.fastq.gz {i}R2.fastq.gz;
 done
 
 # At the end of the process "topout" folder will be generated for each sample contains multiple files.

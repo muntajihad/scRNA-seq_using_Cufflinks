36c7c38d9f89d9c262a2ed671b13bad4bba961b7
muntajihad <73559402+muntajihad@users.noreply.github.com>
2021-12-20 22:54:33 +0300

    Update cffquant.sh

--- cffquant.sh
+++ cffquant.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# working directory is composed of "topout" folders for each samplels
+
 gtf=r1=/path/to/GENCODE.gtf
 
-cuffquant --no-update-check -p 8 -o cell_01_cuffqout $gtf cell_01_topout/accepted_hits.bam
-cuffquant --no-update-check -p 8 -o cell_02_cuffqout $gtf cell_02_topout/accepted_hits.bam
-cuffquant --no-update-check -p 8 -o cell_03_cuffqout $gtf cell_03_topout/accepted_hits.bam
-cuffquant --no-update-check -p 8 -o cell_04_cuffqout $gtf cell_04_topout/accepted_hits.bam
+for i in ls *:
+do
+cuffquant --no-update-check -p 8 -o ${i}_cuffqout $gtf ${i}/accepted_hits.bam
 
-# for each sample a"cuffqout" folder that contains "abundance.cxb" will be generated ()
+
+# for each sample a "cuffqout" folder that contains "abundance.cxb" will be generated ()
 # the path to abundance.cxb of each sample will be used in sample_sheet.txt
 # next use cuffnorm
